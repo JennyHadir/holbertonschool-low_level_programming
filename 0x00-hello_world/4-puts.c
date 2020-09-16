2350b7e47a4c9e27a869cfe9d851efe3e8b74e3d
JennyHadir <hadirjenny@hotmail.com>
2020-09-16 13:42:08 +0200

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
  * Return: Always 0 (Success)
  */
 int main()
-{ puts ("\" Programming is like building a multilingual puzzle");
+{ puts ("\"Programming is like building a multilingual puzzle");
     return (0);
   
 }
