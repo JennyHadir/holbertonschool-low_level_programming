d2cff8814f6a11cb1b4af8097cc1f97e1f1dc27e
JennyHadir <hadirjenny@hotmail.com>
2020-09-21 15:52:05 +0200

    task6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _abs - compute the absolut value of an integer
+ *
+ * @r: parametre from 6-main.c
+ * Return: absolut value
+ */
+int _abs(int r)
+{
+	if (r < 0)
+	{
+		return (r * -1);
+	}
+	else
+	{
+		return (r);
+	}
+}
