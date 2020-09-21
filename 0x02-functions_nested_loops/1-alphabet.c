469bb83e2525f1db8836a6c7a40a1b13df108391
JennyHadir <hadirjenny@hotmail.com>
2020-09-21 12:53:53 +0200

    task1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,17 @@
-#include <stdio.h>
+#include "holberton.h"
 
 /**
-* print_alphabet - function
-* Return: 0
-*/
+ * print_alphabet - alphabet displaying function
+ *
+ * Return 0
+ */
 void print_alphabet(void)
 {
-int i;
-
-for (i = 97; i < 123; i++)
-{
-_putchar(i);
-}
-putchar('\n');
+	int i;
+	
+	for (i = 97; i < 123; i++)
+	{
+		_putchar (i);
+	}
+	_putchar ('\n');
 }
