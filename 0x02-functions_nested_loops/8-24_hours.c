b4eebf662203dba8d4a3d50de121f59493837f23
JennyHadir <hadirjenny@hotmail.com>
2020-09-22 10:35:54 +0200

    task8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * jack_bauer - time function
+ *
+ * Return:
+ */
+void jack_bauer(void)
+{
+	int hr, mn;
+
+	for (hr = 0; hr < 24; hr++)
+	{
+		for (mn = 0; mn < 60; mn++)
+		{
+			_putchar((hr / 10) + '0');
+			_putchar((hr % 10) + '0');
+			_putchar(':');
+			_putchar((mn / 10) + '0');
+			_putchar((mn % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
