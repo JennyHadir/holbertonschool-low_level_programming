562ddd6bab134726da52267ac68e56ffead942be
JennyHadir <hadirjenny@hotmail.com>
2020-09-22 11:16:28 +0200

    task10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * add - add two integer function
+ *
+ * Return: the value of two integer
+ * @x: integer from 10-main.c
+ * @y: integer from 10-main.c
+ */
+int add(int x, int y)
+{
+	int i;
+
+	i = x + y;
+	return (i);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * times_table - time table function
+ *
+ * Return:
+ */
+void times_table(void)
+{
+
+
+	for (i = 0; i <=9; i++)
