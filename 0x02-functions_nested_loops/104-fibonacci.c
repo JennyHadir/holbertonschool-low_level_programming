55b78682d797a923a31f545dc5f87919e9294357
JennyHadir <hadirjenny@hotmail.com>
2020-09-23 10:48:34 +0200

    fixed

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -12,16 +12,15 @@ int main(void)
 	y = 1;
 	x = 1;
 	z = 1;
-	for (i = 1; i < 98; i++)
+	for (i = 1; i <= 98; i++)
 	{
-		printf("%ld, ", z);
-		z = y + x;
 		printf("%ld", z);
+		z = y + x;
+		x = y;
+		y = z;
 		if (i != 98)
 		{
 			printf(", ");
-			x = y;
-			y = z;
 		}
 	}
 	printf("\n");
