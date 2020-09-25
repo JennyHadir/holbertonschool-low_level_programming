d9ba04d396f84008db4b89f84df0a6a72b04a163
JennyHadir <hadirjenny@hotmail.com>
2020-09-25 23:50:10 +0200

    task101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,38 +7,41 @@
 void print_number(int n)
 {
 	int n1, n2, n3, n4;
-
-	if (n < 0)
+	
+	if (n > 1000)
 	{
-		n = n * -1;
+		n1 = n / 1000;
+		n2 = (n % 1000) / 100;
+		n3 = (n / 10) % 10;
+		n4 = n % 10;
+		_putchar(n1 + '0');
+		_putchar(n2 + '0');
+		_putchar(n3 + '0');
+		_putchar(n4 + '0');
 	}
-		if (n > 1000)
-		{
-			n1 = n / 1000;
-			n2 = (n % 1000) / 100;
-			n3 = (n / 10) % 10;
-			n4 = n % 10;
-			_putchar(n1 + '0');
-			_putchar(n2 + '0');
-			_putchar(n3 + '0');
-			_putchar(n4 + '0');
-		}
-		else if (n > 100)
-		{
-			n1 = n / 100;
-			n2 = (n / 10) % 10;
-			n3 = n % 10;
-			_putchar(n1 + '0');
-			_putchar(n2 + '0');
-			_putchar(n3 + '0');
-		}
-		else if (n > 10)
-		{
-			_putchar((n / 10) + '0');
-			_putchar((n % 10) + '0');
-		}
-		else
-		{
-			_putchar(n + '0');
-		}
+	else if (n > 100)
+	{
+		n1 = n / 100;
+		n2 = (n / 10) % 10;
+		n3 = n % 10;
+		_putchar(n1 + '0');
+		_putchar(n2 + '0');
+		_putchar(n3 + '0');
+	}
+	else if (n > 10)
+	{
+		_putchar((n / 10) + '0');
+		_putchar((n % 10) + '0');
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		n = n * -1;
+		_putchar(n / 10 + '0');
+		_putchar((n % 10) + '0');
+	}
+	else
+	{
+		_putchar(n + '0');
+	}
 }
