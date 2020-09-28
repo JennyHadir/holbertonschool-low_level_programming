c2d2999b6622168169984565fe3d7a601deead57
JennyHadir <hadirjenny@hotmail.com>
2020-09-28 11:40:11 +0200

    task2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _strlen - function that calculate the length of a string
+ *
+ * @s: char
+ * Return: length of s
+ */
+int _strlen(char *s)
+{
+	int x = 0;
+
+	while (*s)
+	{
+		x++;
+		s++;
+	}
+	return (x);
+}
