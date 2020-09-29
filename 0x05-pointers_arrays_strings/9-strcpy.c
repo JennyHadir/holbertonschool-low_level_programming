84c4afd86c858b77a9d060dfae5bab7a484f143c
JennyHadir <hadirjenny@hotmail.com>
2020-09-29 17:13:36 +0200

    task9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _strcpy - copy function
+ *
+ * @dest: string destination
+ * @src: string source
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
