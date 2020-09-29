4c38de5d5f9b98851643f3b0ab50e723d03f0d4e
JennyHadir <hadirjenny@hotmail.com>
2020-09-29 11:16:13 +0200

    task4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * print_rev - print in reverse function
+ *
+ * @s: char
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i, n;
+
+	n = 0;
+	while (s[n] != '\0')
+	{
+		n++;
+	}
+	for (i = n; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
