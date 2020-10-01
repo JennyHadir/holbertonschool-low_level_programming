f56c4e5729ea871d6e533c94c10e4725c3aabb6a
JennyHadir <hadirjenny@hotmail.com>
2020-10-01 14:41:30 +0200

    task5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * string_toupper - upper case function
+ *
+ * @s: string
+ * Return: char
+ */
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 32;
+		}
+		i++;
+	}
+	return (s);
+}
