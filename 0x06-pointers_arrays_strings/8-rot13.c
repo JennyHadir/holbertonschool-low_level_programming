e04ee125735ebf42ad672598d388d46a354340dd
JennyHadir <hadirjenny@hotmail.com>
2020-10-01 22:56:05 +0200

    task8

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -8,20 +8,17 @@
 char *rot13(char *s)
 {
 	int i, j;
-	char a[] = "AaBbCcDdEeFfGgHhIiJjKkLlMm";
-	char b[] = "NnOoPpQqRrSsTtUuVvWwXxYyZz";
+	char a[] = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+	char b[] = "NnOoPpQqRrSsTtUuVvWwXxYyZzAaBbCcDdEeFfGgHhIiJjKkLlMm";
 
 	for (i = 0; s[i]; i++)
 	{
-		for (j = 0; j < 26; j++)
+		for (j = 0; j < 52; j++)
 		{
 			if (s[i] == a[j])
 			{
 				s[i] = b[j];
-			}
-			else if (s[i] == b[j])
-			{
-				s[i] = a[j];
+				break;
 			}
 		}
 	}
