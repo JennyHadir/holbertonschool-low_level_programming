fb7517b9c0af6267807d0c643b97727afdac9352
JennyHadir <hadirjenny@hotmail.com>
2020-10-05 21:20:41 +0100

    final_task

--- 0x07-pointers_arrays_strings/9-set_string.c
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -2,8 +2,8 @@
 /**
  * set_string - set the value of a pointer to a char
  *
- * @s : pointer to pointer
- * @to pointer to char
+ * @s: pointer to pointer
+ * @to: pointer to char
  * Return: void
  */
 void set_string(char **s, char *to)
