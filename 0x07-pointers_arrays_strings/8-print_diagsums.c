a2722756b5b4ee45ab96ff8b41f29d4f5a7b7519
JennyHadir <hadirjenny@hotmail.com>
2020-10-05 21:11:52 +0100

    task7

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print _diagsums - print the sum of a diagonal function
+ *
+ * @a: array
+ * @size: integer
+ * Return: void
+ */
+void print_diagsums(int *a, int size)
+{
+  int i, s1 = 0, s2 = 0;
+
+for (i = 0; i < size; ++i)
+{
+s1 += *(a + i * (size + 1));
+s2 += *(a + (size - 1) * i + (size - 1));
+}
+ printf("%d, %d\n", s1, s2);
+}
