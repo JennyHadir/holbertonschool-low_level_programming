ab8803591992745c064a5bdc10d10e664ea8a127
JennyHadir <hadirjenny@hotmail.com>
2020-10-07 15:20:07 +0100

    task0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,7 +7,7 @@
  */
 void _puts_recursion(char *s)
 {
-int i = 0;
+unsigned int i = 0;
 
 if (s[i])
 {
---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * _pow_recursion - power function
+ *
+ * @x: integer
+ * @y: integer
+ * Return: integer
+ */
+int _pow_recursion(int x, int y)
+{
+  if (y == 0)
+{
+  return(1);
+}
+  else
+{
+  if (y > 0)
+{
+return (x * x);
+	y--;
+	_pow_recursion(x, y);
+}
+  else
+{
+return (x * (- x));
+  y--;
+  _pow_recursion(x, y);
+ }
+}
+}
