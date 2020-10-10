3fa4b74c9b5a53bd476cd0497fcdab09ffa5c691
JennyHadir <hadirjenny@hotmail.com>
2020-10-10 19:48:25 +0100

    update

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,14 +9,13 @@
  */
 int main(int argc, char **argv)
 {
+int mulp;
 if (argc < 3)
 {
 printf("ERROR\n");
 return (1);
 }
-else
-{
-printf("%d\n", atoi(argv[2]) * atoi(argv[1]));
+mulp =  atoi(argv[2]) * atoi(argv[1]);
+printf("%d\n", mulp);
 return (0);
-}
 }
