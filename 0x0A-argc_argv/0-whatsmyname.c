b53f129a11834fbf5845deb80987b5421253ca22
JennyHadir <hadirjenny@hotmail.com>
2020-10-10 19:36:11 +0100

    update

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,8 +6,8 @@
  * @argv: array contains the argument value
  * Return: 0
  */
-int main(__attribute__ ((unused))int argc, char **argv)
+int main(__attribute__ ((unused))int argc, char *argv[])
 {
-printf("%s\n", *argv);
+printf("%s\n", argv[0]);
 return (0);
 }
