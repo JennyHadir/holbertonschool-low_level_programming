8ae20826ed463e669fe4ab0f9d263e7993c59f1e
JennyHadir <hadirjenny@hotmail.com>
2020-10-11 14:38:27 +0100

    update

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,7 +9,8 @@
  */
 int main(int argc, char **argv)
 {
-int cents, change = 0;
+int cents, i, change = 0;
+int bills[] = {25, 10, 5, 2, 1};
 if (argc != 2)
 {
 printf("Error\n");
@@ -17,23 +18,13 @@ return (1); }
 cents = atoi(argv[1]);
 if (cents < 1)
 printf("%d\n", 0);
-while (cents != 0)
+if (cents != 0)
 {
-if (cents >= 25)
-{change += cents / 25;
-cents %= 25; }
-else if (cents >= 10)
-{change += cents / 10;
-cents %= 10; }
-else if (cents >= 5)
-{change += cents / 5;
-cents %= 5; }
-else if (cents >= 2)
-{change += cents / 2;
-cents %= 2; }
-else
-{change++;
-cents = 0; }
+for (i = 0; i < 5; i++)
+{
+change += cents / bills[i];
+cents %= bills[i];
+}
 }
 printf("%d\n", change);
 return (0);
