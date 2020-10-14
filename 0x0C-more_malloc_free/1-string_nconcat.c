9cae406d5be548e363df402428ceed7b368e3aa8
JennyHadir <hadirjenny@hotmail.com>
2020-10-14 22:45:37 +0100

    tasks

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,15 +11,15 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *s3;
-unsigned int len1, len2, i = 0;
+unsigned int len1 = 0, len2 = 0, i = 0;
 if (s1 != NULL)
 {
-  for (len1 = 0; s1[len1]; len1++)
+for (; s1[len1]; len1++)
 { ; }
 }
 if (s2 != NULL)
 {
-for (len2 = 0; s2[len2]; len2++)
+for (; s2[len2]; len2++)
 { ; }
 }
 if (n >= len2)
@@ -33,6 +33,7 @@ s3 = malloc(sizeof(char) * (len1 + n + 1));
 while (i < len1)
 {
 s3[i] = s1[i];
+i++;
 }
 while (i < len1 + n)
 {
