b9ad5ee8ba14b2f496f0c1e6aa3f07930b325286
JennyHadir <hadirjenny@hotmail.com>
2020-10-19 21:40:31 +0100

    tasks

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ *free_dog - free dog function
+ *@d: dog struct
+ *Return: void
+ */
+void free_dog(dog_t *d)
+{
+if (d != NULL)
+{
+free(d->name);
+free(d->owner);
+free(d);
+}
+}
