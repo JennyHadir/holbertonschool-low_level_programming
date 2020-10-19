6d84bdae3effb722ce1c17ae489c9bd5ba97e7e3
JennyHadir <hadirjenny@hotmail.com>
2020-10-19 16:45:02 +0100

    tasks

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ *struct dog - description
+ *@name: dog name
+ *@age: dog age
+ *@owner: dog owner
+ */
+typedef struct dog
+{
+  char *name;
+  float age;
+  char *owner;
+}
+#endif
