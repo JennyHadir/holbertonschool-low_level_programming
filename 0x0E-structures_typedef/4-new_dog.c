d8883e3306345365088199029d18079c067da344
JennyHadir <hadirjenny@hotmail.com>
2020-10-19 20:00:48 +0100

    tasks

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,37 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ *new_dog - create new dog function
+ *@name: dog name
+ *@age: dog age
+ *@owner: god owner
+ * Return: struct
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+dog_t *d;
+int len;
+if (!name || !owner)
+return (NULL);
+d = malloc(sizeof(dog_t));
+if (d == NULL)
+return (NULL);
+d->age = age;
+for (len = 0; name[len]; len++)
+;
+d->name = malloc(len * sizeof(char));
+if (d->name == NULL)
+return (NULL);
+for (len = 0; name[len]; len++)
+d->name[len] = name[len];
+d->name[len + 1] = '\0';
+for (len = 0; owner[len]; len++)
+;
+d->owner = malloc(len * sizeof(char));
+if (d->owner == NULL)
+return (NULL);
+for (len = 0; owner[len]; len++)
+d->owner[len] = owner[len];
+d->owner[len + 1] = '\0';
+return (d);
+}
