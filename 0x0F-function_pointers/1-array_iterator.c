2bbe8646773b6ec1756ce70164a6e9d73b4f1cf7
JennyHadir <hadirjenny@hotmail.com>
2020-10-21 19:47:41 +0100

    tasks

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - execute a function of array
+ *@size: size of array
+ *@array: array
+ *@action: function to execute
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+size_t i;
+if (array && action)
+{
+for (i = 0; i < size; i++)
+{
+action(array[i]);
+}
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,4 +1,5 @@
 #ifndef FUNCTION_FILE_H
 #define FUNCTION_FILE_H
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
