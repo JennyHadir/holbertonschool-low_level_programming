4e32c5400f82b80f634fd0b4fc513e55ce10f228
JennyHadir <hadirjenny@hotmail.com>
2020-10-26 16:58:13 +0100

    tasks

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,14 +4,4 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-/**
- * struct tp - pointer to print all
- * @check: pointer
- * @f: pointer to function
- */
-typedef struct tp
-{
-  char *check;
-  void (*f)();
-} tp;
 #endif
