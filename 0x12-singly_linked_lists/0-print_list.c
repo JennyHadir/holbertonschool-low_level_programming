9eb07aeab19601079d8814df221557206ce950c0
JennyHadir <hadirjenny@hotmail.com>
2020-10-30 15:16:38 +0100

    tasks

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -5,16 +5,6 @@
  *@h: linked list
  *Return: number of nodes
  */
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include "lists.h"
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
 size_t print_list(const list_t *h)
 {
 const list_t *lk;
