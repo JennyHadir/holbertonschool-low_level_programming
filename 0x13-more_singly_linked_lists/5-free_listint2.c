5a23e1662752b6f663333be0a07554b4381b23a7
JennyHadir <hadirjenny@hotmail.com>
2020-11-04 03:26:57 +0100

    fix check10

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,13 +8,12 @@
  */
 void free_listint2(listint_t **head)
 {
-listint_t *temp, *temp1;
-temp = *head;
-while (temp)
+listint_t *temp;
+while (*head)
 {
-temp1 = temp;
-temp = temp->next;
-free(temp1);
+temp = *head;
+*head = (*head)->next;
+free(temp);
 }
 head = NULL;
 }
