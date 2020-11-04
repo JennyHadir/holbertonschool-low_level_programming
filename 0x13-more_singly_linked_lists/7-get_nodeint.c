e3d3b729398e198da6638f2ea2da68e0d72f3210
JennyHadir <hadirjenny@hotmail.com>
2020-11-04 08:49:43 +0100

    check 10

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,12 +9,20 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-listint_t *temp;
-unsigned int i;
+listint_t *temp, *temp1;
+unsigned int i, length = 0;
 if (head == NULL)
 return (NULL);
+temp1 = head;
+while (temp1)
+{
+length++;
+temp1 = temp1->next;
+}
+if (index > length)
+return (NULL);
 temp = head;
-for (i = 0; i < index; i++)
+for (i = 0; i < index && temp; i++)
 temp = temp->next;
 return (temp);
 }
