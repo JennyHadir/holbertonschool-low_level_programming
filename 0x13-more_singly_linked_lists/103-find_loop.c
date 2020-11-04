ad3ba0bde4f45844fed6c45bd9d7316bc2c98c7b
JennyHadir <hadirjenny@hotmail.com>
2020-11-04 09:46:50 +0100

    fix code

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -14,9 +14,9 @@ return (NULL);
 slow_p = head;
 fast_p = head;
 while (slow_p && fast_p && fast_p->next)
-{ 
-slow_p = slow_p->next; 
-fast_p = fast_p->next->next; 
+{
+slow_p = slow_p->next;
+fast_p = fast_p->next->next;
 if (fast_p == slow_p)
 {
 slow_p = head;
