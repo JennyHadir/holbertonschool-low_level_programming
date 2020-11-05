3dd25dd10762dd5f0bd3d5f5d8f50d2c81d63a51
JennyHadir <hadirjenny@hotmail.com>
2020-11-05 15:01:20 +0100

    fix check 13

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,14 +8,18 @@
 unsigned int binary_to_uint(const char *b)
 {
 unsigned int result = 0;
-int i = 0;
+int i;
 if (b == NULL)
 return (0);
-while (b[i] == '0' || b[i] == '1')
+for (i = 0; b[i]; i++)
+{
+if (b[i] != '0' && b[i] != '1')
+return (0);
+}
+for (i = 0; b[i]; i++)
 {
 result <<= 1;
 result += b[i] - '0';
-i++;
 }
 return (result);
 }
