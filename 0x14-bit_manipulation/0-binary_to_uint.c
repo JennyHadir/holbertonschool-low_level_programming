a53ac8b6e4c2c45a73630e963e679fa807d46863
JennyHadir <hadirjenny@hotmail.com>
2020-11-05 14:53:38 +0100

    fix checker

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,17 +7,15 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int result = 0, multip = 1;
-int i;
+unsigned int result = 0;
+int i = 0;
 if (b == NULL)
 return (0);
-for (i = strlen(b) - 1; i >= 0; i--)
+while (b[i] == '0' || b[i] == '1')
 {
-if (b[i] != '1' && b[i] != '0')
-return (0);
-if (b[i] == '1')
-result += multip;
-multip *= 2;
+result <<= 1;
+result += b[i] - '0';
+i++;
 }
 return (result);
 }
