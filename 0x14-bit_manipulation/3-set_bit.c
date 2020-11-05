c7285aa9b70284314b8da1cfe42af9d9035a1d27
JennyHadir <hadirjenny@hotmail.com>
2020-11-05 22:42:21 +0100

    first commit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -8,8 +8,9 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
+unsigned long int i = 1;
 if (n == NULL || (sizeof(unsigned long int) * 8))
 return (-1);
-*n ^= (1 << index);
+*n |= (i << index);
 return (1);
 }
